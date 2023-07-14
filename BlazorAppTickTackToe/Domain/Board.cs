0ab134626f91e36176a12359f06b03849732b726
rodion-m <rodion.mostovoy@outlook.com>
2023-07-14 16:19:36 +0600

    Добавлено отображения ячеек в цикле; А также обработка события клика по ячейке

--- BlazorAppTickTackToe/Domain/Board.cs
+++ BlazorAppTickTackToe/Domain/Board.cs
@@ -10,6 +10,14 @@
         public int ColumnCount => 3;
         public int RowCount => 3;
 
+        public CellState[,] Cells { get; set; }
+
+        public Board()
+        {
+            Cells = new CellState[RowCount, ColumnCount];
+        }
+
+
         public CellState Cell_0_0 { get; set; } = CellState.X;
         public CellState Cell_0_1 { get; set; }
         public CellState Cell_0_2 { get; set; }
